9cf80c78fa3ecbf18fdf5a485f22c50ff5092d10
Sch <junhg0211@gmail.com>
2021-12-21 19:28:18 +0900

    oh wait I made a mistake

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name='sat-datetime',  # How you named your package folder (MyLib)
-    packages=['sat-datetime'],  # Chose the same as "name"
-    version='0.1',  # Start with a small number and increase it with every change you make
+    name='sat_datetime',  # How you named your package folder (MyLib)
+    packages=['sat_datetime'],  # Chose the same as "name"
+    version='0.2',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Sat universe time calculation',  # Give a short description about your library
     author='Sch Jeon',  # Type in your name
