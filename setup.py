b0fc81372a1a88aebd9dd6c5a8921c4436619a9a
Sch <junhg0211@gmail.com>
2022-09-29 18:23:43 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Sch Jeon',  # Type in your name
     author_email='junhg0211@gmail.com',  # Type in your E-Mail
     url='https://github.com/junhg0211/sat-datetime',  # Provide either the link to your github or to your website
-    download_url='https://github.com/junhg0211/sat-datetime/archive/refs/tags/v_0.4.tar.gz',  # I explain this later on
+    download_url='https://github.com/Zeli-B/sat-datetime/archive/refs/tags/v_1.0.tar.gz',  # I explain this later on
     keywords=['sat', 'datetime'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'validators',
